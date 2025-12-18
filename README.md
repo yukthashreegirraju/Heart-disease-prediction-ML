9ee895ef3f578def5d242c3b38104808c6ae5360
yukthashreegirraju <yukthas790@gmail.com>
2025-12-18 20:46:49 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Heart-disease-prediction-ML
+
---
+++ src/main.py
@@ -0,0 +1,39 @@
+import pandas as pd
+import pickle
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+
+# Load dataset
+data = pd.read_csv("../data/heart.csv")
+
+# Optional: see first 5 rows
+print(data.head())
+
+# Features & target
+X = data.drop("target", axis=1)
+y = data["target"]
+
+# Scale features
+scaler = StandardScaler()
+X_scaled = scaler.fit_transform(X)
+
+# Split dataset
+X_train, X_test, y_train, y_test = train_test_split(
+    X_scaled, y, test_size=0.2, random_state=42
+)
+
+# Train model
+model = RandomForestClassifier(n_estimators=100, random_state=42)
+model.fit(X_train, y_train)
+
+# Evaluate
+y_pred = model.predict(X_test)
+print("Accuracy:", accuracy_score(y_test, y_pred))
+
+# Save model
+with open("../model/heart_model.pkl", "wb") as f:
+    pickle.dump((model, scaler), f)
+
+print("Model saved successfully")
